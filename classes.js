588fddae9dd0b1d3e41e1bf721f9e86828dd8b16
vk-varun <97547208+vk-varun@users.noreply.github.com>
2024-10-29 05:42:39 -0600

    classes list JS file

---
+++ classes.js
@@ -0,0 +1,47 @@
+let classes = [
+    {
+        id: 1001,
+        title: "Art Class",
+        description: "Explore your creativity in this fun and engaging art class.",
+        price: 150.00,
+        image: "images/art-class.jpg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        id: 1002,
+        title: "Music Class",
+        description: "Learn to play an instrument or improve your singing skills.",
+        price: 200.00,
+        image: "images/music-class.jpg",
+        availableInventory: 5,
+        rating: 4
+    },
+    {
+        id: 1003,
+        title: "Dance Class",
+        description: "Get moving and grooving in this exciting dance class.",
+        price: 100.00,
+        image: "images/dance-class.jpg",
+        availableInventory: 5,
+        rating: 5
+    },
+    {
+        id: 1004,
+        title: "Cooking Class",
+        description: "Learn to cook delicious meals with our expert chefs.",
+        price: 250.00,
+        image: "images/cooking-class.jpg",
+        availableInventory: 5,
+        rating: 2
+    },
+    {
+        id: 1005,
+        title: "Fitness Class",
+        description: "Get in shape and stay healthy with our fitness classes.",
+        price: 150.00,
+        image: "images/fitness-class.jpg",
+        availableInventory: 5,
+        rating: 4
+    }
+]
